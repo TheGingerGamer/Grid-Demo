0843424e64103745571c40d8847d1d8634332f0c
TheGingerGamer <143351023+TheGingerGamer@users.noreply.github.com>
2024-09-09 12:31:54 -0500

    Outline update
    Tiles can only be highlighted when a player is selected to move.

--- Strategy RPG/Assets/Scripts/OutlineSelection.cs
+++ Strategy RPG/Assets/Scripts/OutlineSelection.cs
@@ -9,35 +9,88 @@ public class OutlineSelection : MonoBehaviour
     private Transform highlight;
     private Transform selection;
     private RaycastHit raycastHit;
+    private bool playerTurn;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerTurn = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(highlight != null){
+        if(highlight != null)
+        {
             highlight.gameObject.GetComponent<Outline>().enabled = false;
             highlight = null;
         }
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if(!EventSystem.current.IsPointerOverGameObject() && Physics.Raycast(ray, out raycastHit)){
+        if(!EventSystem.current.IsPointerOverGameObject() && Physics.Raycast(ray, out raycastHit))
+        {
             highlight = raycastHit.transform;
-            if (highlight.CompareTag("Tile") && highlight != selection){
-                if(highlight.gameObject.GetComponent<Outline>() != null){
+            if (highlight.CompareTag("Tile") && playerTurn)
+            {
+                if(highlight.gameObject.GetComponent<Outline>() != null)
+                {
                     highlight.gameObject.GetComponent<Outline>().enabled = true;
-                } else{
+                } 
+                else
+                {
                     Outline outline = highlight.gameObject.AddComponent<Outline>();
                     outline.enabled = true;
-                    highlight.gameObject.GetComponent<Outline>().OutlineColor = Color.white;
-                    highlight.gameObject.GetComponent<Outline>().OutlineWidth = 3.0f;
+                    highlight.gameObject.GetComponent<Outline>().OutlineColor = Color.yellow;
+                    highlight.gameObject.GetComponent<Outline>().OutlineWidth = 7.0f;
+                    
                 }
-            } else{
+            } 
+            else if (highlight.CompareTag("Player"))
+            {
+                if(highlight.gameObject.GetComponent<Outline>() != null)
+                {
+                    highlight.gameObject.GetComponent<Outline>().enabled = true;
+                } 
+                else
+                {
+                    Outline outline = highlight.gameObject.AddComponent<Outline>();
+                    outline.enabled = true;
+                    highlight.gameObject.GetComponent<Outline>().OutlineColor = Color.blue;
+                    highlight.gameObject.GetComponent<Outline>().OutlineWidth = 7.0f;
+                }
+            } 
+            else
+            {
                 highlight = null;
             }
         }
 
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(highlight)
+            {
+                if(selection != null)
+                {
+                    selection.gameObject.GetComponent<Outline>().enabled = true;
+                }
+                selection = raycastHit.transform;
+                if(selection.CompareTag("Player") && !playerTurn)
+                {
+                    playerTurn = true;
+                    selection.gameObject.GetComponent<Outline>().enabled = true;
+                    highlight = null;
+                }
+                else
+                {
+                    playerTurn = false;
+                }
+            }
+            else
+            {
+                if(selection)
+                {
+                    selection.gameObject.GetComponent<Outline>().enabled = false;
+                    selection = null;
+                }
+            }
+        }
     }
 }
